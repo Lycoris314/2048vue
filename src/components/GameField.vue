1dd4ca6ede5317c3c6c248a549edcbb5ebf3e8b5
Lycoris314 <159458009+Lycoris314@users.noreply.github.com>
2024-04-20 11:36:45 +0900

    preventDefault

--- src/components/GameField.vue
+++ src/components/GameField.vue
@@ -56,14 +56,17 @@ onMounted(() => {
 
     let startX: number, endX: number, startY: number, endY: number
     html.addEventListener("touchstart", (e) => {
+        e.preventDefault
         startX = e.touches[0].pageX;
         startY = e.touches[0].pageY;
     })
     html.addEventListener("touchmove", (e) => {
+        e.preventDefault
         endX = e.changedTouches[0].pageX;
         endY = e.changedTouches[0].pageY;
     })
     html.addEventListener("touchend", (e) => {
+        e.preventDefault
         if (preventEventCondition.value) return;
 
         function calcDir(dY: number, dX: number) {
